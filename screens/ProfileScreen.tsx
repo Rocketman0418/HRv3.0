615e1d69d39cb0dc65bff1f8ca918d3e9d8da59f
Clay Speakman <clay@healthrocket.life>
2025-09-15 16:33:56 -0400

    Fix Sign Out button by restoring Alert import

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -7,6 +7,7 @@ import {
   SafeAreaView,
   TouchableOpacity,
   Alert,
+  Alert,
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useAuth } from '../contexts/AuthContext';
